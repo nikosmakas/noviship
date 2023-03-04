82056b4f05b1247ff7b828f19833c101783472f0
nikosmakas <nikosmakas@protonmail.com>
2023-03-04 02:04:44 +0200

    add search and popup buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'noviship';
+
+  results: any[] = [];
+  openPopup(){
+    let params = `scrollbars=no,resizable=yes,status=no,location=no,toolbar=no,menubar=no,
+width=600,height=600,left=100,top=100`;
+
+  open('/geiasas','hello',params) ;
+  }
+
+
+  searchMovie(){}
+
 }
