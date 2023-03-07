b9f67b9f5ebb0d9b1d170313ebd3e356a8d63452
nikosmakas <nikosmakas@protonmail.com>
2023-03-07 23:24:44 +0200

    validator actually validates

--- src/app/validator.directive.ts
+++ src/app/validator.directive.ts
@@ -9,17 +9,6 @@ export class ValidatorDirective implements Validator {
   
   @Input() forbiddenChars!: RegExp;
 
-  /*validate(control: AbstractControl): ValidationErrors | null {
-    if (!control.value || control.value.length < 3) {
-      return { minlength: true };
-    }
-
-    if (this.forbiddenChars && this.forbiddenChars.test(control.value)) {
-      return { forbidden: true };
-    }
-
-    return null;
-  } */
   validate(control: AbstractControl): { [key: string]: any } | null {
     const forbidden = /[^a-zA-Z0-9 ]+/;
     if (!control.value || control.value.length < 3 || forbidden.test(control.value)) {
